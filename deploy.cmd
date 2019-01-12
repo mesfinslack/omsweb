f7a1fe156f4b2885a7c6f234a83ab5166f2954dd
Mesfin Bali <MesfinBa@ethiopianairlines.com>
2019-01-12 19:35:37 +0300

    4 commit omswebapp

--- deploy.cmd
+++ deploy.cmd
@@ -99,7 +99,7 @@ IF EXIST "%DEPLOYMENT_SOURCE%\package.json" (
   popd
 )
 :: 3. Angular Prod Build
-IF EXIST "%DEPLOYMENT_SOURCE%/.angular-cli.json" (
+IF EXIST "%DEPLOYMENT_SOURCE%/.angular.json" (
 echo Building App in %DEPLOYMENT_SOURCE%…
 pushd "%DEPLOYMENT_SOURCE%"
 call :ExecuteCmd !NPM_CMD! run build
